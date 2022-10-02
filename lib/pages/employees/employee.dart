9f6f582295557dd5160f123e86e7be1ef7de7a3f
pineda24 <85385878+pineda24@users.noreply.github.com>
2022-10-02 15:04:07 -0600

    empl

--- lib/pages/employees/employee.dart
+++ lib/pages/employees/employee.dart
@@ -18,15 +18,7 @@ class _EmployeeState extends State<Employee> {
       List.generate(7, (int index) => TextEditingController());
   DateTime _date = DateTime.now();
 
-  void getDataEdit() async {
-    var url = Uri.https('example.com', 'whatsit/create');
-    var response =
-        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-
-    print(await http.read(Uri.https('example.com', 'foobar.txt')));
-  }
+  void getDataEdit() async {}
 
   @override
   Widget build(BuildContext context) {
