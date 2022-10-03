c5577ed95eba2b758d254cad22fab94775549b60
pineda24 <85385878+pineda24@users.noreply.github.com>
2022-10-03 17:11:50 -0600

    models update

--- lib/pages/employees/employees_list.dart
+++ lib/pages/employees/employees_list.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:scottmanager/pages/employees/employee.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,10 +15,27 @@ class EmployeeList extends StatefulWidget {
 }
 
 class _EmployeeListState extends State<EmployeeList> {
+  List<Widget> listEmployees = [];
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+  }
+
+  Future<void> getData() async {
+    // Response res = await get(Uri.http('localhost:3000', 'team'));
+    // if (res.statusCode == 200) {
+    //   List<dynamic> aux = jsonDecode(res.body);
+    //   List<Team> teams = [];
+    //   for (var i = 0; i < aux.length; i++) {
+    //     teams.add(Team.fromJson(aux[i]));
+    //   }
+
+    //   return teams;
+    // } else {
+    //   throw "Unable to retrieve posts.";
+    // }
   }
 
   void getEmployeesList() async {
@@ -30,8 +50,33 @@ class _EmployeeListState extends State<EmployeeList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: listEmployees(context),
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return Dismissible(
+          // Each Dismissible must contain a Key. Keys allow Flutter to
+          // uniquely identify widgets.
+          key: Key("${index}"),
+          // Provide a function that tells the app
+          // what to do after an item has been swiped away.
+          onDismissed: (direction) {
+            // Remove the item from the data source.
+            setState(() {
+              items.removeAt(index);
+            });
+
+            // Then show a snackbar.
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text('$item dismissed')));
+          },
+          // Show a red background as the item is swiped away.
+          background: Container(color: Colors.red),
+          child: ListTile(
+            title: Text(item),
+          ),
+        );
+      },
     );
   }
 
